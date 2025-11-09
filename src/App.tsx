da9e065a04ec46fda41d12aa4442e9ff2ff679e2
sonu jaiswal <sonunpj2023@gmail.com>
2025-11-09 13:06:42 +0900

    Added comments to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,42 @@
+import myPhoto from "./assets/me.jpg";
+
+// App component: Main component of the application
 function App() {
   return (
     <div className="text-center p-10">
-      <h1 className="text-3xl font-bold text-blue-500">Hello, I am sonu jaiswal</h1>
+      {/* Page Title */}
+      <h1 className="text-3xl font-bold text-blue-500">
+        Hello, I am Sonu Jaiswal
+      </h1>
 
-      <p className="mt-3 text-lg">Welcome to my first React + Tailwind Webpage 🎉 
-        </p>
-        <p className="mt-3 text-lg">I am a student of Web System in KCGI College, I am studying Web Programming from Michal Macinka Sir. 
-        </p>
+      {/* Short welcome message */}
+      <p className="mt-3 text-lg">
+        Welcome to my first React + Tailwind Webpage 🎉
+      </p>
+
+      {/* Additional introduction */}
+      <p className="mt-3 text-lg">
+        I am a student of Web System in KCGI College. I am studying Web Programming under Michal Macinka Sir.
+      </p>
+
+      {/* List of personal info */}
       <ul className="mt-4 list-disc mx-auto w-fit text-left">
-        <li>My favorite food</li> momos,pizza,ramen
-        <li>My hobby</li>reading,traveling,cooking
-        <li>My favorite color</li>red,blue
+        <li>
+          My favorite food: momos, pizza, ramen
+        </li>
+        <li>
+          My hobby: reading, traveling, cooking
+        </li>
+        <li>
+          My favorite color: red, blue
+        </li>
       </ul>
 
+      {/* Display personal image */}
       <img
-        className="mt-5 w-40 rounded-lg border"
-        src="https://picsum.photos/200"
-        alt="Random"
+        src={myPhoto}
+        alt="My personal photo"
+        className="w-40 h-40 rounded-lg object-cover mt-4"
       />
     </div>
   );
